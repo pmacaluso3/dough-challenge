5f09d6cbe47547908e38d0af85845a4d6c8bf273
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-09-07 18:41:25 -0500

    rake task 'import' is populating database

--- lib/importer/symbol_name_importer.rb
+++ lib/importer/symbol_name_importer.rb
@@ -14,19 +14,18 @@ class SymbolNameImporter
     raw_text = File.read(filename)
     no_literal_quotes = raw_text.gsub("\"", "")
     split_lines = no_literal_quotes.split(/[\r\n]+/)
-    p split_lines.map {|l| l.split(",")}
+    split_lines.map {|l| l.split(",")}
   end
 
   def build_company_objects
     array_of_arrays.each do |company_array|
       stock_symbol = company_array[0]
       name = company_array[1]
-      # this_company = Company.find_or_initialize_by(stock_symbol: stock_symbol)
-
+      this_company = Company.find_or_initialize_by(stock_symbol: stock_symbol)
+      if this_company.new_record?
+        this_company.name = name
+        this_company.save
+      end
     end
-    # this_company
   end
-
 end
-
-# SymbolNameImporter.new('../../db/company_csvs/amex.csv').import
