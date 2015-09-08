db2c7a83c8553538b44882fb6964c3dddef1ace5
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-09-08 12:36:55 -0500

    chart appears for a company; resolved y-axis bug

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -19,18 +19,18 @@ class CompaniesController < ApplicationController
     yahoo_client = YahooFinance::Client.new
     @company = Company.find_by(id: params[:id])
     unaveraged_data = yahoo_client.historical_quotes(@company.stock_symbol, {start_date: Time::now - 50.days, end_date: Time::now})
-    @averaged_days = unaveraged_data.map do |day|
+    averaged_days = unaveraged_data.map do |day|
       [day.open.to_f, day.close.to_f, day.high.to_f, day.low.to_f].reduce(:+)/4.0
-    end.first(30)
-    days = unaveraged_data.map{|d|d.trade_date}
-    price_range = [@averaged_days.min/1.25, @averaged_days.max*1.25]
+    end.first(30).reverse
+    days = unaveraged_data.map{|d|d.trade_date}.reverse
+    price_range = [0, averaged_days.max, averaged_days.max/10]
     @chart =  Gchart.line(
               size: '500x500',
               title: @company.name,
               bg: {color: 'cccccc', type: 'gradient'},
-              data: @averaged_days,
+              data: averaged_days, max_value: averaged_days.max,
               axis_with_labels: ['x', 'y'],
               axis_range: [[1,11,21,30],price_range],
-              axis_labels: [[days[0], days[10], days[20], days.last]])
+              axis_labels: [[days[0], days[10], days[20], 'Today']])
   end
 end
