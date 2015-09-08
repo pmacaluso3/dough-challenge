90c515f60113cbf7eea68b4d11bab840d8e3afdf
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-09-07 20:17:00 -0500

    company index is displaying searched for companies:

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,7 +8,11 @@ class CompaniesController < ApplicationController
   end
 
   def search
-
+    companies_by_name = Company.where("LOWER(name) LIKE ?", "#{params[:query].downcase}%")
+    companies_by_stock_symbol = Company.where("LOWER(stock_symbol) LIKE ?", "#{params[:query].downcase}%")
+    unsorted_companies = companies_by_name.concat(companies_by_stock_symbol)
+    @companies = unsorted_companies.sort {|a, b| a.name <=> b.name }.first(20)
+    render :"companies/index"
   end
 
   def show
